5c20e1086f5477b280ee814595e23d196ac32f58
Mayank Panwar <mayankpanwar066@gmail.com>
2025-06-18 01:13:42 +0530

    img change

--- src/App.jsx
+++ src/App.jsx
@@ -428,7 +428,7 @@ const App = () => {
                   />
                   <img
                     className="rounded-3xl w-[550px] rotate-[-10deg] object-cover shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]"
-                    src={chat1}
+                    src={chat2}
                     alt=""
                   />
                   <img
